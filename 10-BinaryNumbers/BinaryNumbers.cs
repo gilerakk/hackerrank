3c2b4f4edae194f4e0d0ecc8f5d30d171f36b947
Radek Plocr <radek.plocr@gmail.com>
2018-03-28 22:07:01 +0200

    BinaryNumber final

--- 10-BinaryNumbers/BinaryNumbers.cs
+++ 10-BinaryNumbers/BinaryNumbers.cs
@@ -10,7 +10,44 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
+            int remainder = 0;
+            List<int> list = new List<int>();
+            string result = "";
+            int consecutive = 0;
+            int maxValue = 0;
             
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n > 0)
+            {
+                remainder = n % 2;
+
+                if (remainder == 1)
+                {
+                    consecutive++;
+                    maxValue = Math.Max(maxValue, consecutive);
+                                        
+                }
+                else
+                {
+                    consecutive = 0;
+                }
+                
+
+                n = n / 2;
+                list.Add(remainder);
+
+
+            }
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                result += list[list.Count - i - 1];
+            }
+
+            Console.WriteLine("Vypocet: " + result);
+            Console.WriteLine("Consecutive: " + maxValue);
+            Console.ReadLine();
+
         }
     }
 }
