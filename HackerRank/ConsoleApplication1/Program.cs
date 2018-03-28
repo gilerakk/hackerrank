39771049d8e3d1a38af066c6b064057b5570bbfc
Radek Plocr <gilerakk@volny.cz>
2018-03-28 17:18:25 +0200

    prvni verze

---
+++ HackerRank/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Solution
+    {
+
+        static int factorial(int n)
+        {
+            if (n >= 2 && n <= 12)
+            {
+                int result = n;
+
+                while (n > 1)
+                {
+                    result = result * (n - 1);
+                    n--; 
+                }
+
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = factorial(n);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
