d1582c64f3e57b3c9fe11bdc6d26720cf612a681
Radek Plocr <radek.plocr@gmail.com>
2018-03-30 17:18:51 +0200

    14-Scope pridano

---
+++ 14-Scope/Scope.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Difference
+    {
+        private int[] elements;
+        public int maximumDifference;
+        // Add your code here
+        public Difference(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void computeDifference()
+        {
+            int max = 0;
+            int min = 0;
+
+            max = elements.Max();
+            min = elements.Min();
+
+
+            /*
+            //hledani v poli za pomoci LINQ
+            var vysledek = from element in elements
+                        where element > 100
+                        select element;
+            */
+
+            maximumDifference = max - min;          
+
+        }
+
+
+
+    } // End of Difference Class
+
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
+            Console.ReadKey();
+        }
+    }
+}
