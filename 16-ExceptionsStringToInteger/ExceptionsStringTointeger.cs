d4598ec0b41d6ea53ce47b1b08120387cf248c58
Radek Plocr <radek.plocr@gmail.com>
2018-03-30 18:59:54 +0200

    ExceptionStringToInteger pridano

---
+++ 16-ExceptionsStringToInteger/ExceptionsStringTointeger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej string, ktery chces prevest na cislo:");
+            string S = Console.ReadLine();
+            int result = 0;
+
+            try
+            {
+                result = int.Parse(S);
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Bad String");
+                Console.ReadKey();
+            }
+
+            /*
+            if (int.TryParse(S, out result))
+            {
+                Console.WriteLine(result);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Bad String");
+                Console.ReadKey();
+            }
+            */
+        }
+    }
+}
