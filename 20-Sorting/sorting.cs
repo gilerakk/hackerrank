6874ed75b79b40802ece6a39249293d5c0001430
Radek Plocr <radek.plocr@gmail.com>
2018-03-31 12:12:42 +0200

    20-Sorting pridano

---
+++ 20-Sorting/sorting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+            // Write Your Code Here
+
+            int numberOfSwaps = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                
+                for (int j = 0; j < a.Length - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int tmp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = tmp;
+                        numberOfSwaps++;
+                    }
+                }
+            }
+
+            
+
+            Console.WriteLine("Array is sorted in {0} swaps.", numberOfSwaps);
+            Console.WriteLine("First Element: {0}", a[0]);
+            Console.WriteLine("Last Element: {0}", a[a.Length - 1]);
+            Console.ReadKey();
+
+
+        }
+    }
+}
