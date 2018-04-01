65dbdaf4827b7091a22dfd2867e39b068e90ac60
Radek Plocr <radek.plocr@gmail.com>
2018-04-01 13:18:07 +0200

    26-NestedLogic pridano

---
+++ 25-RunningTimeAndComplexity/RunningTimeAndComplexity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            //Console.WriteLine("Zadej pocet prvku: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+                        
+            int[] arr = new int[n];
+            //Console.WriteLine("Zadej cisla - na kazdy radek jedno: ");
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] =  Convert.ToInt32(Console.ReadLine());
+            }
+                        
+            foreach (int item in arr)
+            {
+                bool prime = true;
+                int counter = 2; //deleni jednickou se nepocita, takze zacinam dvojkou
+                //jednicka je not prime
+                if (item == 1)
+                {
+                    prime = false;
+                }
+                while (counter*counter <= item)
+                {
+                    if (item % counter == 0)
+                    {
+                        prime = false;
+                        break;
+                    }
+                    counter++;
+                }
+
+                //Console.WriteLine("Pocet iteraci: " + counter);
+
+                if (prime == false)
+                {
+                    Console.WriteLine("Not prime");
+                }
+                else
+                {
+                    Console.WriteLine("Prime");
+                }
+            }
+            
+
+            for (int i = 2; i*i <= n; i++)
+            {
+
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ 26-NestedLogic/NestedLogic.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+            string[] arrActual = Console.ReadLine().Split(' ');
+            DateTime actualDate = new DateTime(Convert.ToInt16(arrActual[2]), Convert.ToInt16(arrActual[1]), Convert.ToInt16(arrActual[0]));
+
+            string[] arrExpected = Console.ReadLine().Split(' ');
+            DateTime expectedDate = new DateTime(Convert.ToInt16(arrExpected[2]), Convert.ToInt16(arrExpected[1]), Convert.ToInt16(arrExpected[0]));
+
+            //Moznost A
+            /*
+            if (actualDate.Year > expectedDate.Year)
+            {
+                Console.WriteLine("10000");
+            }
+            else if ((actualDate.Month > expectedDate.Month) && (actualDate.Ticks >= expectedDate.Ticks))
+            {
+                int result = 500 * (actualDate.Month - expectedDate.Month);
+                Console.WriteLine(result);
+            }
+            else if (actualDate.Day > expectedDate.Day && (actualDate.Ticks >= expectedDate.Ticks))
+            {
+                int result = 15 * (actualDate.Day - expectedDate.Day);
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+            */
+
+            //Moznost B
+            if (actualDate.Year == expectedDate.Year)
+            {
+                if (actualDate.Month > expectedDate.Month)
+                {
+                    int result = 500 * (actualDate.Month - expectedDate.Month);
+                    Console.WriteLine(result);
+                }
+                else if (actualDate.Month == expectedDate.Month && actualDate.Day > expectedDate.Day)
+                {
+                    int result = 15 * (actualDate.Day - expectedDate.Day);
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("0");
+                }
+            }
+            else if (actualDate.Year > expectedDate.Year)
+            {
+                Console.WriteLine("10000");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+                        
+            Console.ReadKey();
+            
+        }
+    }
+}
