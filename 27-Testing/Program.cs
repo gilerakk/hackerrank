eed2732d8df3f3aa3b8f21cf37eb1e6d618a2446
Radek Plocr <radek.plocr@gmail.com>
2018-04-01 13:58:14 +0200

    27-Testing pridano

---
+++ 27-Testing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Solution
+    {                       
+
+    static void Main(string[] args)
+        {
+            Console.WriteLine("5");
+
+            Console.WriteLine("3 3");
+            Console.WriteLine("0 1 -1");
+
+            Console.WriteLine("4 3");
+            Console.WriteLine("0 1 -1 -1");
+
+            Console.WriteLine("5 3");
+            Console.WriteLine("0 1 -1 1 1");
+
+            Console.WriteLine("6 3");
+            Console.WriteLine("0 1 -1 -1 -1 -1");
+
+            Console.WriteLine("7 3");
+            Console.WriteLine("0 1 -1 1 1 1 1");
+
+        }
+    }
+}
